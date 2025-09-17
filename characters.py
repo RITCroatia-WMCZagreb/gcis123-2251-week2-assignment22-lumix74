ad7d0d640ea1805ff8dc8d18c365c3af27a02586
Lazar Jovovic <lumix@Lazars-MacBook-Pro.local>
2025-09-17 23:49:44 +0200

    homework

--- characters.py
+++ characters.py
@@ -1,7 +1,43 @@
 '''
-@ASSESSME.USERID: userID
+@ASSESSME.USERID: lj7829
 @ASSESSME.AUTHOR: author, list of authors
 @ASSESSME.DESCRIPTION: Assignment 2.2
 @ASSESSME.ANALYZE: YES
 '''
+
+def add_chars(char1, char2):
+    sum_value = ord(char1) + ord(char2)
+    print(f"Sum of ASCII values: {sum_value}")
+    if 0 <= sum_value <= 127:
+        print(f"Characters for sum: {chr(sum_value)}")
+    else:
+        print("Sum is out of valid ASCII range")
+
+def subtract_chars(char1, char2):
+    diff_value = ord(char1) - ord(char2)
+    print(f"Sum of ASCII values: {diff_value}")
+    if 0 <= diff_value <= 127:
+        print(f"Characters for difference: {chr(diff_value)}")
+    else:
+        print("Difference is out of valid ASCII range")
+
+def main():
+    char1 = input("Enter the first character: ")
+    char2 = input("Enter the second character: ")
+
+    if len(char1) != 1 or len(char2) != 1:
+        print("Error")
+        return
+    
+    add_chars(char1, char2)
+    subtract_chars(char1, char2)
+
+if __name__ == "__main__":
+    main()
+
+    #Comment#1: Sometimes odd characters or nothing appear because the result of adding or subtracting ASCII values can be outside the printable ASCII range (0-127)
+    #When that happens, the characters can't be displayed.
+
+    #Comment#2: It gives error because it works with only single character.
+    #That's why the program checks the input length and shows an error message if it's not valid.
 
--- scope.py
+++ scope.py
@@ -1,6 +1,35 @@
 '''
-@ASSESSME.USERID: userID
+@ASSESSME.USERID: lj7829
 @ASSESSME.AUTHOR: author, list of authors
 @ASSESSME.DESCRIPTION: Assignment 2.2
 @ASSESSME.ANALYZE: YES
 '''
+
+STRING_GLOBAL = "Hello, world!"
+INT_GLOBAL = 10
+FLOAT_GLOBAL = 2.5
+
+def print_param(param):
+    print(f"Parameter value: {param}")
+
+def print_local():
+    local_var = "local variable"
+    print(f"Local variable: {local_var}")
+
+def print_which():
+    STRING_GLOBAL = "local string"
+    print(f"String global inside print_which(): {STRING_GLOBAL}")
+
+def main():
+    print_param(STRING_GLOBAL)
+    print_param(INT_GLOBAL)
+    print_param(FLOAT_GLOBAL)
+
+    local_var = "Main local"
+    print_local()
+
+    print_which()
+    print(f"STRING_GLOBAL in main(): {STRING_GLOBAL}")
+
+if __name__ == "__main__":
+    main()
